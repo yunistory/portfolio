75bcd31db11beea701b214d14842352d07be4247
yunistory <134893745+yunistory@users.noreply.github.com>
2023-06-27 15:29:50 +0900

    Add files via upload

--- js/script-timeline.js
+++ js/script-timeline.js
@@ -1,7 +1,7 @@
 $(function () {
   // 애니메이션 시킬 대상을 변수로 저장
-  const $gnb = $(".gnb > li");
-  const $img3d = $(".img-3d");
+  const $gnb = $('.gnb > li');
+  const $img3d = $('.img-3d');
 
   // 애니메이션의 타임라인 설정(여러 요소를 순차적으로 애니메이션 시키기 위해서)
   const mainAni = gsap.timeline();
@@ -15,6 +15,6 @@ $(function () {
       delay: 0.5,
       stagger: 0.2,
     })
-    .from($img3d, { x: 50, duration: 3 })
-    .from($img3d, { y: 20, delay: 1 });
+    .from($img3d, { x: 50, duration: 1 });
+  // .from($img3d, { y: 20 });
 });
