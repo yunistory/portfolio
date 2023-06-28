d50bbfc8749310c0296504b23226bd92308b46aa
yunistory <134893745+yunistory@users.noreply.github.com>
2023-06-28 12:48:47 +0900

    Add files via upload

---
+++ script-loading.js
@@ -0,0 +1,21 @@
+$(function () {
+  const $win = $(window);
+  const $body = $('body');
+
+  let $loading = `<div class="loading">
+    <div class="ring">Loading
+    <span></span>
+    </div>
+  </div>`;
+
+  $body.append($loading);
+  console.log($body);
+  $loading = $('.loading');
+
+  $win.on('load', function () {
+    // setTimeout(동작, 시간);
+    setTimeout(function () {
+      $loading.fadeOut().remove();
+    }, 100);
+  });
+});
